a4335283d1c72a78f92a652cfad13e93737b901d
Rosi <fatchurrosi17@gmail.com>
2026-05-31 13:04:34 +0700

    start uvicorn

--- ocr-service/main.py
+++ ocr-service/main.py
@@ -8,3 +8,13 @@ app = FastAPI()
 def root():
     print("ROOT HIT")
     return {"hello": "world"}
+
+if __name__ == "__main__":
+    import os
+    import uvicorn
+
+    uvicorn.run(
+        app,
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 8080))
+    )
